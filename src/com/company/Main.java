3d778b3ba594e37a120e53afd31b567853f8519d
Armani Raleigh <armanijolee@gmail.com>
2016-10-12 10:11:40 -0400

    Intializing Lab 22 Project

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Grant Chirpus's Code Dojo! We offer a variety of classes to pave the way for an exciting caeering in technology. " +
+                "Enter enroll at any time to join a class!");
+        System.out.println();
+        System.out.println("Our classes include: ");
+        System.out.println("Hard Skills: ");
+        System.out.println( "=====================");
+        System.out.println("Java (Open)");
+        System.out.println("JavaScript (Open)");
+        System.out.println("Web Development ");
+        System.out.println("Software Testing (Open)");
+
+    }
+}
